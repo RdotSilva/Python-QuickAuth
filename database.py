8a7c667a5b521e56a05da827a2d1cb65df70cc07
RdotSilva <RyandotSilva@gmail.com>
2022-05-22 12:44:52 +0100

    Add database setup file

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./auth.db"
+
+# Create the database engine
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={
+        "check_same_thread": False
+    },  # connect_args may not be needed when changing from SQLite
+)
+
+# Create a session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a base that will create the database model and we will inherit from
+Base = declarative_base()

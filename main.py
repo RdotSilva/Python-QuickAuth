dc2446fd98781ca56d46963281a5b3e3f2db3370
RdotSilva <RyandotSilva@gmail.com>
2022-05-24 19:53:45 +0100

    Add route for fetching all tasks

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
 from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -8,10 +9,16 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-# Create a local database session
+# Create a local database session to pass into API routes
 def get_db():
     try:
         db = SessionLocal()
         yield db
     finally:
         db.close()
+
+
+# Read all tasks
+@app.get("/")
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Tasks).all()

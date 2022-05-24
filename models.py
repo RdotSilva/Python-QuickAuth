dd585041b718a089168416ff0d98494574b15a8c
RdotSilva <RyandotSilva@gmail.com>
2022-05-24 19:28:56 +0100

    Add tasks model

--- models.py
+++ models.py
@@ -1,4 +1,12 @@
 from sqlalchemy import Boolean, Column, Integer, String
 from database import Base
 
-# TODO: Add user model
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)

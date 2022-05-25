6722c7fd498737c35e933826f739d599bdbdb5fb
RdotSilva <RyandotSilva@gmail.com>
2022-05-25 17:51:01 +0100

    Add get route for fetching task by ID

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -22,3 +22,13 @@ def get_db():
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Tasks).all()
+
+
+# Read task based on ID
+@app.get("/task/{task_id}")
+async def read_task(task_id: int, db: Session = Depends(get_db)):
+    task_model = db.query(models.Tasks).filter(models.Tasks.id == task_id).first()
+
+    if task_model is not None:
+        return task_model
+    raise HTTPException(status_code=404, detail="Task not found")

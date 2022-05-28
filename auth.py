15240616fd8fe967aeef3984ef460dd01d5db1c2
RdotSilva <RyandotSilva@gmail.com>
2022-05-28 16:37:48 +0100

    Add CreateUser base model

--- auth.py
+++ auth.py
@@ -1,1 +1,15 @@
-# TODO: Create user
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+import models
+
+
+class CreateUser(BaseModel):
+    username: str
+    email: Optional[str]
+    first_name: str
+    last_name: str
+    password: str
+
+
+app = FastAPI()

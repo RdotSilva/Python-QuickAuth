920162ef23c5597dd5ce8c82456118aded4ca168
RdotSilva <RyandotSilva@gmail.com>
2022-06-07 20:17:18 +0100

    Add successful_task_created_response

--- responses.py
+++ responses.py
@@ -6,3 +6,8 @@ def successful_response(status_code: int):
 
     """
     return {"status": status_code, "transaction": "Successful"}
+
+
+def successful_task_created_response():
+    """Generate a successful response including a 201 status code and message when a task is successfully created"""
+    return {"status": 201, "transaction": "Successfully created task"}

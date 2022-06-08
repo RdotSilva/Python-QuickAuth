cb52a8d14f16a73520dda3787c41189da1864bcc
RdotSilva <RyandotSilva@gmail.com>
2022-06-08 19:26:53 +0100

    Add successful_task_updated_response

--- responses.py
+++ responses.py
@@ -11,3 +11,8 @@ def successful_response(status_code: int):
 def successful_task_created_response():
     """Generate a successful response including a 201 status code and message when a task is successfully created"""
     return {"status": 201, "transaction": "Successfully created task"}
+
+
+def successful_task_updated_response():
+    """Generate a successful response including a 200 status code and message when a task is successfully updated"""
+    return {"status": 200, "transaction": "Successfully updated task"}
